eb9a81b505b42deae2950670705d18f3cfe26a9e
akmaralAW <akmaral.moldagalieva1@ulaval.ca>
2023-01-13 17:02:22 +0100

    dataset.yaml format inference output file

--- inference.py
+++ inference.py
@@ -8,13 +8,17 @@ from utils import *
 import yaml
 import shutil
 import os
-import cv2
+import cv2, argparse
+from pathlib import Path
 # Tested with single Cf case
 def testing_locanet():
-    shutil.rmtree(cfg.DATASET_FOLDER + '../locanet/prediction/', ignore_errors=True)
-    os.mkdir(cfg.DATASET_FOLDER + '../locanet/prediction/')
-    xyz_loca = []
-    prediction={}
+    parser = argparse.ArgumentParser()
+    parser.add_argument("foldername")
+    args = parser.parse_args()
+    folder = Path(args.foldername)
+     
+    shutil.rmtree(str(folder) + '/locanet/prediction/', ignore_errors=True)
+    os.mkdir(str(folder) + '/locanet/prediction/')
     dist = 5
     testset = Dataset_Test(cfg) 
     locanet_weights = cfg.WEIGHT_PATH + cfg.OUTPUT_FILE
@@ -25,7 +29,9 @@ def testing_locanet():
     model_locanet = tf.keras.Model(input_layer, feature_maps_locanet)
     model_locanet.load_weights(locanet_weights)
     start = perf_counter()
+    predictions, images = {},{}
     for image_name, image_data, target in testset:
+        pred_neighbors, per_image = [], {}
         # predict with locanet
         pred_result_locanet = model_locanet(image_data, training=False)
         conf_locanet = tf.sigmoid(pred_result_locanet[0, :, :, 1:2]) 
@@ -41,21 +47,26 @@ def testing_locanet():
                 x_loca = (tf.exp(pred_result_locanet[0, xy[0], xy[1], 0])).numpy() # depth
                 y_loca = -x_loca*(curW-160)/170
                 z_loca = -x_loca*(curH-160)/170  # params used for data generation
-                xyz_loca.append(np.array((x_loca,y_loca,z_loca)).tolist())
+                pred_neighbors.append(np.array([x_loca,y_loca,z_loca]))
+            if len(pred_neighbors):
+                # shutil.copy(path + robot_name[i] + '/' + img_names[t], synch_data_path + img_names[t])
+                all_robots = {}
+                for h in range(len(pred_neighbors)):
+                    per_robot = {}
+                    per_robot['pos'] = pred_neighbors[h].tolist() 
+                    all_robots[h] = per_robot
+                per_image['visible_neighbors'] = all_robots
+                images[image_name[0]] = per_image
+                # visualize predictions
                 cv2.rectangle(img, (int(curW), int(curH)), (int(curW), int(curH)), (0, 0, 255), 4)
+                cv2.imwrite(os.path.join(cfg.DATASET_FOLDER+ '../locanet/prediction/', image_name[0]), img)
 
-            prediction[str(image_name[0])] = xyz_loca[:]
-            del xyz_loca[:] 
-            cv2.imwrite(os.path.join(cfg.DATASET_FOLDER+ '../locanet/prediction/', image_name[0]), img)
-
-        else:
-            prediction[str(image_name[0])] = np.array((None,None,None)).tolist()
-        
-
+    predictions['images'] = images
     with open(cfg.INFERENCE_FILE, 'w') as outfile:
-        yaml.dump(prediction, outfile)
+        yaml.dump(predictions, outfile)
     end = perf_counter()
     print("Time taken for test is {} min.".format((end-start)/60.))
+
 
 if __name__ == "__main__":
     testing_locanet()
