12fd07d630ea3da34a83ee98eab5e12637019c6a
akmaralAW <moldagalieva@tu-berlin.de>
2023-02-15 14:22:11 +0100

    abs. number training data split

--- get_locanet_labels.py
+++ get_locanet_labels.py
@@ -8,11 +8,9 @@ import glob
 import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
-from os.path import normpath, basename
 # Converts dataset.yaml into locanet training format. Images with no robot has just image-name
 # python3 get_locanet_labels.py PATH-TO-MAIN-FOLDER
-def run(main_data_folder, img_ext, train_data_percentage):
-
+def run(main_data_folder, img_ext, train_data_percentage, data_split):
     synchronized_data_folder = main_data_folder + 'Synchronized-Dataset/'
     yaml_path = synchronized_data_folder + 'dataset.yaml'
     locanet_folder = main_data_folder + 'locanet/'
@@ -21,54 +19,49 @@ def run(main_data_folder, img_ext, train_data_percentage):
     
     with open(yaml_path, 'r') as stream:
         synchronized_data = yaml.safe_load(stream)
-    files = ['train', 'val', 'test']   
-    train_robot_number, test_robot_number = np.zeros(6), np.zeros(6)
-    for folder in [f.path for f in os.scandir(synchronized_data_folder) if f.is_dir()]:
-        robot_number = basename(normpath(folder)) # get the folder with robot number
-        total_imgs = sorted(filter( os.path.isfile, glob.glob(folder + '/' + img_ext) ) )
-        # Prepare training, validation, testing data
-        indices = list(range(0, len(total_imgs))) 
-        numImgTrain = round(train_data_percentage/100*len(total_imgs))
-        training_idx, val_idx, test_idx = indices[:numImgTrain], indices[numImgTrain:numImgTrain+int((len(total_imgs)-numImgTrain)/2)], indices[numImgTrain+int((len(total_imgs)-numImgTrain)/2):]
-        train_robot_number[int(robot_number)] += len(training_idx)
-        test_robot_number[int(robot_number)] += len(test_idx)
-        idx = [training_idx,val_idx,test_idx]
-        for k in range(len(idx)): # train, val, test
-            file_name = locanet_folder + files[k] + '.txt'
-            fileTmp = open(file_name, 'a')
-            for t in idx[k]: # for each image
-                dataLine = str(robot_number) + '/' + total_imgs[t].split("/")[-1] + ' ' # 1/img_name
-                for j in range(len(synchronized_data['images'][str(robot_number) + '/' + total_imgs[t].split("/")[-1]]['visible_neighbors'])):
-                    pix = synchronized_data['images'][str(robot_number) + '/' + total_imgs[t].split("/")[-1]]['visible_neighbors'][j]['pix']
-                    pos = synchronized_data['images'][str(robot_number) + '/' + total_imgs[t].split("/")[-1]]['visible_neighbors'][j]['pos']
-                    dataLine += str(int(pix[0])) + ',' + str(int(pix[1])) + ',' + str(round(pos[0]*1000)) + ',' + str(round(pos[1]*1000)) + ',' + str(round(pos[2]*1000)) + ' ' # m->mm
-                dataLine += '\n'
-                fileTmp.write(dataLine)
+    files = ['train', 'val']   
+    # for folder in [f.path for f in os.scandir(synchronized_data_folder) if f.is_dir()]:
+    for key in data_split: # for each folder 0,1,2
+        total_imgs = sorted(filter(os.path.isfile, glob.glob(synchronized_data_folder + key + '/' + img_ext)))
+        if (len(total_imgs)) or data_split[key] <= len(total_imgs):
+            indices = list(range(0, data_split[key])) 
+            numImgTrain = round(train_data_percentage/100*len(indices))
+            training_idx, val_idx = indices[:numImgTrain], indices[numImgTrain:]
+            idx = [training_idx,val_idx]
+            for k in range(len(idx)): # train, val
+                file_name = locanet_folder + files[k] + '.txt'
+                fileTmp = open(file_name, 'a')
+                for t in idx[k]: # for each image
+                    dataLine = key + '/' + total_imgs[t].split("/")[-1] + ' ' # 1/img_name
+                    for j in range(len(synchronized_data['images'][key + '/' + total_imgs[t].split("/")[-1]]['visible_neighbors'])):
+                        pix = synchronized_data['images'][key + '/' + total_imgs[t].split("/")[-1]]['visible_neighbors'][j]['pix']
+                        pos = synchronized_data['images'][key + '/' + total_imgs[t].split("/")[-1]]['visible_neighbors'][j]['pos']
+                        dataLine += str(int(pix[0])) + ',' + str(int(pix[1])) + ',' + str(round(pos[0]*1000)) + ',' + str(round(pos[1]*1000)) + ',' + str(round(pos[2]*1000)) + ' ' # m->mm
+                    dataLine += '\n'
+                    fileTmp.write(dataLine)
+        else:
+            print('Not Enough Images in folder {}'.format(key))
+            return 
 
-    # Training, Testing number of robots
-    fig, axs = plt.subplots(1, 2, sharey=True, sharex=True)
-    fig.suptitle('Used Number of Robots')
-    axs[0].set_title("Training", fontsize=10)
-    axs[0].bar([*range(0, len(train_robot_number.tolist()), 1)], train_robot_number.tolist())
-    axs[1].set_title("Testing", fontsize=10)
-    axs[1].bar([*range(0, len(test_robot_number.tolist()), 1)], test_robot_number.tolist())
-    fig.text(0.5, 0.04, 'Number of Images', va='center', ha='center')
-    fig.text(0.04, 0.5, 'Number of Robots', va='center', ha='center', rotation='vertical')
-    plt.savefig(main_data_folder + 'train_test_robot_number.jpg')
+    plt.title('Training Dataset')
+    plt.bar(list(data_split.keys()), data_split.values(), color='g')
+    plt.ylabel('Number of Images')
+    plt.xlabel('Number of Robots')
+    plt.savefig(main_data_folder + 'train_robot_number.jpg')
     
-        
 def parse_opt():
     parser = argparse.ArgumentParser()
     parser.add_argument('foldername', help="Path to the Synchronized-Dataset folder")
     parser.add_argument('--img_ext', type=str, default= '*.jpg', help="image extension") 
-    parser.add_argument('--training_data', type=int, default=90, help='training data percentage')
-
+    parser.add_argument('--training_data_percentage', type=int, default=90, help='training data percentage')
+    parser.add_argument('--data_split', default={'0': 10, '1':50, '2': 100}, help='percentage for data split between different number of robots')
 
     args = parser.parse_args()
     return args
 
 def main(args):
-    run(args.foldername, args.img_ext, args.training_data)
+    run(args.foldername, args.img_ext, args.training_data_percentage, args.data_split)
+    
 
 
 if __name__ == "__main__":
