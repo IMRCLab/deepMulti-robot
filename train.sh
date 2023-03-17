1c6d568a43765e0d48a6072c63df1b9869d30dc3
akmaralAW <moldagalieva@tu-berlin.de>
2023-03-17 10:01:46 +0100

    rm conda env

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 # source ~/.bashrc
-mv /home/whoenig/sw/miniconda3_ /home/whoenig/sw/miniconda3
-source ~/sw/miniconda3/etc/profile.d/conda.sh
-conda activate tf
+# mv /home/whoenig/sw/miniconda3_ /home/whoenig/sw/miniconda3
+# source ~/sw/miniconda3/etc/profile.d/conda.sh
+# conda activate tf
 python3 train.py "$@"
-mv /home/whoenig/sw/miniconda3 /home/whoenig/sw/miniconda3_
+# mv /home/whoenig/sw/miniconda3 /home/whoenig/sw/miniconda3_
